2f60ab461bdf402856d4f39adb6dc315961df1e4
Andrei Shpakovskiy <shpakovskiyandrei@gmail.com>
2023-03-21 19:22:08 +0300

    Implement purchases query on Android

--- android/src/main/kotlin/com/shpak/subscription_checker/SubscriptionCheckerPlugin.kt
+++ android/src/main/kotlin/com/shpak/subscription_checker/SubscriptionCheckerPlugin.kt
@@ -1,5 +1,7 @@
 package com.shpak.subscription_checker
 
+import android.content.Context
+import android.util.Log
 import androidx.annotation.NonNull
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -8,24 +10,90 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
-/** SubscriptionCheckerPlugin */
-class SubscriptionCheckerPlugin : FlutterPlugin, MethodCallHandler {
-    private lateinit var channel: MethodChannel
+import com.android.billingclient.api.*
+import com.android.billingclient.api.BillingClient.BillingResponseCode
 
-    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "subscription_checker")
-        channel.setMethodCallHandler(this)
+class SubscriptionCheckerPlugin : FlutterPlugin, MethodCallHandler {
+    companion object {
+        private const val TAG = "SubscriptionChecker"
     }
 
-    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    private lateinit var channel: MethodChannel
+    private lateinit var billingClient: BillingClient
+    private lateinit var context: Context
+
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        context = flutterPluginBinding.applicationContext
+
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "subscription_checker")
+        channel.setMethodCallHandler(this)
+
+        billingClient = BillingClient.newBuilder(context)
+            .enablePendingPurchases()
+            .setListener(PurchasesUpdatedListener { billingResult, purchases ->
+                Log.d(TAG, "Purcheses update: result $billingResult, purchases $purchases")
+            })
+            .build()
+
+        billingClient.startConnection(object : BillingClientStateListener {
+            override fun onBillingSetupFinished(billingResult: BillingResult) {
+                if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+                    Log.d(TAG, "The BillingClient is ready")
+                }
+            }
+
+            override fun onBillingServiceDisconnected() {
+                Log.e(
+                    TAG, "The BillingClient is disconnected, "
+                            + "ensure your device supports Google Play Services"
+                )
+            }
+        })
+    }
+
+    override fun onMethodCall(call: MethodCall, result: Result) {
         if (call.method == "checkSubscription") {
-            result.success("Not implemented yet")
+//            billingClient.queryPurchaseHistoryAsync(BillingClient.ProductType.SUBS) { responseCode, purchasesList ->
+//                Log.d(
+//                    "TAG123",
+//                    ">>> Full purchase history: $responseCode, $purchasesList"
+//                )
+//            }
+
+//                        val params = QueryPurchasesParams.newBuilder()
+//                            .setProductType(ProductType.SUBS)
+//
+//                        val purchasesResult = billingClient.queryPurchasesAsync(params.build())
+
+            val queryPurchasesParams = QueryPurchasesParams
+                .newBuilder()
+                .setProductType(BillingClient.ProductType.SUBS)
+                .build()
+
+            billingClient.queryPurchasesAsync(queryPurchasesParams) { billingResult: BillingResult,
+                                                                      purchases: MutableList<Purchase> ->
+
+                if (billingResult.responseCode == BillingResponseCode.OK) {
+                    Log.d(
+                        TAG,
+                        "QueryPurchasesResponse: ${purchases.first().purchaseTime}; ${billingResult.debugMessage}"
+                    )
+                } else {
+                    result.error(
+                        "$billingResult",
+                        "Error while quering purchases",
+                        billingResult.debugMessage
+                    )
+                }
+            }
+
+            // result.success("Not implemented yet")
         } else {
             result.notImplemented()
         }
     }
 
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
     }
 }
